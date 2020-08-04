48ca517303b30dbc255d43764dc9558071dd091c
Maverick <eugenepashkovsky1@gmail.com>
2020-08-04 10:14:00 +0300

    Are arrow functions odd.js

---
+++ Are arrow functions odd.js
@@ -0,0 +1,6 @@
+// Time to test your basic knowledge in functions! Return the odds from a list:
+// odds([1,2,3,4,5]) #=> [1,3,5]
+
+function odds(values){
+    return values.filter(values => values % 2);
+}
