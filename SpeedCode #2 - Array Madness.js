fded3b34b075fb21b1ce46172a4fb1815d95e4c0
Maverick <eugenepashkovsky1@gmail.com>
2020-08-26 10:45:47 +0300

    SpeedCode #2 - Array Madness

---
+++ SpeedCode #2 - Array Madness.js
@@ -0,0 +1,12 @@
+/*
+SpeedCode #2 - Array Madness
+
+Objective
+
+Given two integer arrays a, b, both of length >= 1, create a program that returns true 
+if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+*/
+
+const arrayMadness = (a, b) => 
+a.reduce((num1, num2) => num1 + num2 ** 2, 0) >
+b.reduce((num1, num2) => num1 + num2 ** 3, 0)
