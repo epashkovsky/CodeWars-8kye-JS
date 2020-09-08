b5412e7e08ef1cba99d17d8ccd3c85e6aa1593d6
Maverick <eugenepashkovsky1@gmail.com>
2020-09-08 13:47:13 +0300

    Semi-Optional

---
+++ Semi-Optional.js
@@ -0,0 +1,6 @@
+/*
+We have implemented a function wrap(value) that takes a value of arbitrary type and wraps 
+it in a new Javascript Object setting the 'value' key on the new Object to the passed-in value.
+*/
+
+const wrap = (value) => ({ value:value });
