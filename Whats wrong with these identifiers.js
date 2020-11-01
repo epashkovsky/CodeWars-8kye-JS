1703ea1e2509f9c5522848dd69ead9796e183b42
Maverick <eugenepashkovsky1@gmail.com>
2020-11-01 22:13:19 +0200

    Whats wrong with these identifiers

---
+++ Whats wrong with these identifiers.js
@@ -0,0 +1,11 @@
+/*
+An identifier is simply a name...
+Can you amend this object so that its properties comprise only vaild identifiers?
+*/
+
+var Person = {
+    firstname: "John",
+    secondname: "Doe",
+    emailddress: "john.doe@email.com",
+    sex: "M"
+};
