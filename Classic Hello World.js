9a92918fe350f89a7ef4dfc0c613981d8827bc68
Maverick <eugenepashkovsky1@gmail.com>
2020-12-21 11:28:08 +0200

    Classic Hello World

---
+++ Classic Hello World.js
@@ -0,0 +1,10 @@
+/*
+You are given a method called main, make it print Hello World! and don't return anything
+Note that for some languages, the function main is the entry point of the program.
+*/
+
+class Solution{
+    static main() {
+      console.log("Hello World!")
+   }
+}
