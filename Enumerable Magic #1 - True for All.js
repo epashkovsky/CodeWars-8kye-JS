186bc3ffc8e6c9f9854618dfd22f2dc9797888b3
Maverick <eugenepashkovsky1@gmail.com>
2021-03-01 22:45:52 +0200

    Enumerable Magic #1 - True for All

---
+++ Enumerable Magic #1 - True for All.js
@@ -0,0 +1,9 @@
+/*
+Create a method all which takes an array and a predicate (function pointer), and returns true 
+if the predicate returns true for every element in the array. Otherwise, it should return false. 
+If the array is empty, it should return true, since technically nothing failed the test.
+
+Here's a (Ruby) resource if you get stuck: http://www.rubycuts.com/enum-all
+*/
+
+const all = ( arr, fun ) => arr.every(fun);
